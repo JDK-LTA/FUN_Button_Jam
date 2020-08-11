c3b605ced0150aa4372820c7a9c7ae7fe4e1e9ff
Salt_Powered <davidtr22@gmail.com>
2020-08-11 12:44:26 +0100

    Ajío

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -10,11 +10,14 @@ public class PlayerController : MonoBehaviour
     public GameObject dworld;
     SpriteRenderer playerRenderer;
     Camera cameraRef;
+    bool isLightworld = true;
+
 
     // Start is called before the first frame update
     void Start()
     {
         playerRenderer = GetComponentInChildren<SpriteRenderer>();
+        ChangeWorld();
     }
 
     // Update is called once per frame
@@ -23,26 +26,31 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown("space"))
         {
             moveHorizontal *= -1;
-            if (moveHorizontal == 1)
-            {
-                dworld.SetActive(false);
-                lworld.SetActive(true);
-                playerRenderer.color = Color.white;
-                Camera.main.backgroundColor = Color.black;
-            }
-            else
-            {
-                dworld.SetActive(true);
-                lworld.SetActive(false);
-                playerRenderer.color = Color.black;
-                Camera.main.backgroundColor = Color.white;
-            }
+            isLightworld = !isLightworld;
+            ChangeWorld();
         }
 
         transform.Translate(Vector3.right * moveHorizontal * Time.deltaTime * speed);
 
     }
 
-    
+    private void ChangeWorld()
+    {
+        if (isLightworld)
+        {
+            dworld.SetActive(false);
+            lworld.SetActive(true);
+            playerRenderer.color = Color.white;
+            Camera.main.backgroundColor = Color.black;
+        }
+        else
+        {
+            dworld.SetActive(true);
+            lworld.SetActive(false);
+            playerRenderer.color = Color.black;
+            Camera.main.backgroundColor = Color.white;
+        }
+    }
+
 }
 
