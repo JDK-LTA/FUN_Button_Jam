20d5c4d4673c51dc9f679c44fa379f82709ff32b
Salt_Powered <davidtr22@gmail.com>
2020-08-12 16:48:06 +0100

    Text

---
+++ Assets/Script/TypewriterStarter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TypewriterStarter : MonoBehaviour
+{
+    TypewriterText textTostart;
+    private void Start()
+    {
+        textTostart = GetComponent<TypewriterText>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerController>())
+        {
+            textTostart.enabled = true;
+            this.enabled = false;
+        }
+    }
+}
