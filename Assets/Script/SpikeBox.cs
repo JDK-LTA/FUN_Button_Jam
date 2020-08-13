33c3d481dc8cd7d28109340dd9f467becd04c05a
Sergio Checa <sergio.checa.dev@hotmail.com>
2020-08-13 12:50:55 +0200

    Spikebox and tutorial level

---
+++ Assets/Script/SpikeBox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeBox : MonoBehaviour
+{
+    Vector3 pointA, pointB;
+    public Transform pointAt, pointBt;
+    Vector3 activePoint;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Instance.OnWorldChanging += SwitchPoint;
+        pointA = pointAt.position;
+        pointB = pointBt.position;
+        activePoint = pointA;
+        
+    }
+
+    
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, activePoint, Time.deltaTime * speed);
+    }
+
+    void SwitchPoint()
+    {
+        if(activePoint == pointA)
+        activePoint = pointB;
+        else
+        {
+            activePoint = pointA;
+        }
+    }
+}
