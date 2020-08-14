36f748f5b2873274ffc4a5cc5fa75e8042500e87
Sergio Checa <sergio.checa.dev@hotmail.com>
2020-08-14 17:18:46 +0200

    diddler

---
+++ Assets/Script/LightScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Instance.OnWorldChanging += LightSwitch;
+    }
+
+    void LightSwitch()
+    {
+        GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+    }
+}
