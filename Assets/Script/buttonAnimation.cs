acc70a8ad1c3a2197adbbf5d9e1c122a61ca6123
Sergio Checa <sergio.checa.dev@hotmail.com>
2020-08-14 17:30:04 +0200

    LMAO

---
+++ Assets/Script/buttonAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonAnimation : MonoBehaviour
+{
+    void Start()
+    {
+        GameManager.Instance.OnWorldChanging += Butt;
+    }
+
+    void Butt()
+    {
+        GetComponent<Animator>().SetTrigger("Press");
+    }
+}
