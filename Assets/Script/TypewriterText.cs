cdfbe00fc957cfa8f4f74467c032545e29e905b1
Salt_Powered <davidtr22@gmail.com>
2020-08-15 11:15:36 +0100

    Build

--- Assets/Script/TypewriterText.cs
+++ Assets/Script/TypewriterText.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using TMPro.Examples;
-using UnityEditor.Experimental.GraphView;
+
 
 public class TypewriterText : MonoBehaviour
 {
